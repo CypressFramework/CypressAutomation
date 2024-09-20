8a43f8a9468a59dd89a766f2e8aa9e7e4fba6039
CypressFramework <ubhedaladinath111@gmail.com>
2024-09-20 18:47:13 +0530

    Testing spec File

--- cypress/e2e/registerTest.cy.js
+++ cypress/e2e/registerTest.cy.js
@@ -9,7 +9,7 @@ it("Register Flow",()=>{
     registerObj.openURL()
     registerObj.enterFirstName(registerData.firstName)
     registerObj.enterlastName(registerData.lastName)
-    registerObj.enterEmail(registerData.email)
+    registerObj.enterEmail(Math.random().toString(5).substring(2)+"@gmail.com")
     registerObj.enterTelephone(registerData.telephone)
     registerObj.enterPassword(registerData.password)
     registerObj.selectCheckbox()
