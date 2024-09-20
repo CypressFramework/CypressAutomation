98047385f2ea4b5000226d63fcfb29257c24ed83
CypressFramework <ubhedaladinath111@gmail.com>
2024-09-20 13:45:46 +0530

    Automation Cypress

---
+++ cypress/e2e/registerTest.cy.js
@@ -0,0 +1,26 @@
+import { registerPage } from "../Pages/RegisterPage";
+const registerObj= new registerPage()
+import registerData from '/home/neosoft/AutomationTesting/CypressFramework/CypressAutomation/cypress/fixtures/registerData.json'
+
+describe("Registering the user",()=>{
+
+it("Register Flow",()=>{
+
+    registerObj.openURL()
+    registerObj.enterFirstName(registerData.firstName)
+    registerObj.enterlastName(registerData.lastName)
+    registerObj.enterEmail(registerData.email)
+    registerObj.enterTelephone(registerData.telephone)
+    registerObj.enterPassword(registerData.password)
+    registerObj.selectCheckbox()
+    registerObj.clickOnContinue()
+
+
+
+
+
+
+})
+
+
+})
